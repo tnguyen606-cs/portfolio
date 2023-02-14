fd0405ed499b7dbd5da050494b97ced762510525
tnguyen606-cs <tnguyen606@gatech.edu>
2023-02-14 12:15:28 -0500

    Updated the Epense App Info

--- src/modals/PortfolioModal4.js
+++ src/modals/PortfolioModal4.js
@@ -6,20 +6,26 @@ export function Modal4() {
     <div className="modal-dialog">
       <div className="container">
         <div className="col-lg-12">
-          <h2 className="text-uppercase">Expense Tracker App</h2>
+          <h2 className="text-uppercase">AI Expense Tracker App</h2>
           <p className="item-intro text-muted">Self project, Present</p>
           <h3 className="h4 mb-4 mt-5">Introduction</h3>
-          <p>A Django web app serving as an expense tracker that helps users track daily, weekly, and monthly expenses through pie charts.</p>
+          <p>An awesome AI-powered expense-saving app to jumpstart that helps users in saving little by little for personal goals by tracking users daily expenses and send summary alerts via SMS message.</p>
           <h3 className="h4 mb-4 mt-5">How to build this app?</h3>
           <ul className="list-inline-item">
               <li>
-                Using React.js, React Hooks for having features of adding, removing, and updating the expenses.
+                Use AI and Machine Learning algorithm (SVM) to answer users’ questions and predict the money flow.
               </li>
               <li>
-                Build a tool to send daily summary alerts via SMS using Python, Plaid and Twilio.
+                Display categories through visible charts that give users a detailed view
               </li>
               <li>
-                Deploy project on multiple hosting services: Heroku, AWS-S3.
+                Use SSO and Google two-factor Authenticator in Python to make users secured authentication. 
+              </li>
+              <li>
+                Build a tool to send daily summary alerts via SMS using Python and Twilio API.
+              </li>
+              <li>
+                Utilize: Python, Flask, PostgreSQL, HTML, CSS, Matplotlib, Twilio API, Heroku, AWS S3.
               </li>
           </ul>
           <Link to="/">
